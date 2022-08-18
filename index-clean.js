93f81a0daf0e636364ab1144f680961a29ce6645
musafir928 <uganoghli@gmail.com>
2022-08-18 14:45:05 +0200

    final touch, clean

--- index-clean.js
+++ index-clean.js
@@ -12,20 +12,11 @@ const {
     color = 'Pink',
     size = 100
 } = argv;
-
-// refactoring step1: use request body/options function to achieve simplicity and reusability
-/**
- * @returns eg: {url: https://cataas.com/cat/says/Hi%20There?width=500&amp;height=800&amp;c=Cyan&amp;s=150}
- */
 const requestBodyGenerator = (word, width, height, color, size) =>
     `https://cataas.com/cat/says/${word}?width=${width}&height=${height}&color=${color}&s=${size}`;
 
-const firstReq = requestBodyGenerator(greeting, width, height, color, size);
-const secondReq = requestBodyGenerator(who, width, height, color, size);
-
-// refactoring step1 end
-
-// refactoring step2: use axios: reason1: npm request deprecated, reason2: avoid callback hell
+const firstReqUrl = requestBodyGenerator(greeting, width, height, color, size);
+const secondReqUrl = requestBodyGenerator(who, width, height, color, size);
 
 const getCatImage = async (requestUrl, requestNo) => {
     try {
@@ -45,17 +36,20 @@ const getCatImage = async (requestUrl, requestNo) => {
     }
 };
 
-// refactoring second step finished
-
 const combineImages = async () => {
     try {
-        const firstBody = await getCatImage(firstReq, 1);
-        const secondBody = await getCatImage(secondReq, 2);
-
         blend(
             [
-                { buffer: Buffer.from(firstBody, 'binary'), x: 0, y: 0 },
-                { buffer: Buffer.from(secondBody, 'binary'), x: width, y: 0 }
+                {
+                    buffer: new Buffer.from(await getCatImage(firstReqUrl, 1), 'binary'),
+                    x: 0,
+                    y: 0
+                },
+                {
+                    buffer: new Buffer.from(await getCatImage(secondReqUrl, 2), 'binary'),
+                    x: width,
+                    y: 0
+                }
             ],
             { width: width * 2, height: height, format: 'jpeg' },
             (err, data) => {
@@ -66,7 +60,6 @@ const combineImages = async () => {
                         console.log(err);
                         return;
                     }
-
                     console.log('The file was saved!');
                 });
             }
